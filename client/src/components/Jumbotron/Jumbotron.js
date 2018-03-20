d0349cc7ddf58ccb371ad6efbb8ff00f0e82ce0c
ShiKwan <31040895+ShiKwan@users.noreply.github.com>
2018-03-20 18:22:33 -0400

    did a yarn build, and added a new nyt logo

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,14 +1,15 @@
 import React from "react";
 import './Jumbotron.css';
-import Background from './images/nyt_banner.png';
+import Background from './images/New_York_Times_Logo.jpg';
 
 var jumbotronStyle = {
   backgroundSize : `contain`,
   backgroundRepeat : `no-repeat`,
   backgroundPosition : `center`,
-  height: 500,
+  height: 300,
   clear: `both`,
-  backgroundImage: `url(${Background})`
+  backgroundImage: `url(${Background})`,
+  backgroundColor: `white`
 }
 
 const Jumbotron = ({ children }) => (
