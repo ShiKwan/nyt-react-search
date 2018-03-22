31b6a0aeae005c955007f18af3ca9e4a9350fcff
Shi-Kwan <sktan@insytecg.com>
2018-03-22 01:18:54 -0400

    removed book related module

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,7 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
 const noteRoutes = require("./notes");
 const articleRoutes = require("./articles");
 
-// Book routes
-router.use("/books", bookRoutes);
 router.use("/notes", noteRoutes);
 router.use("/article", articleRoutes);
 
